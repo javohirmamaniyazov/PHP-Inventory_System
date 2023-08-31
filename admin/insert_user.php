2be9e1a0558b4e9dd81c52bc568344d9ec47e9e2
JavohirMamaniyazov <javohirmamaniyazov@gmail.com>
2023-08-31 14:39:18 +0500

    Given project Successfully started and some section ended

---
+++ admin/insert_user.php
@@ -0,0 +1,139 @@
+<?php
+include('header.php');
+include '../user/connection.php'
+    ?>
+<!--main-container-part-->
+<div id="content">
+    <!--breadcrumbs-->
+    <div id="content-header">
+        <div id="breadcrumb"><a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-user"></i>
+                Add New User</a></div>
+    </div>
+    <!--End-breadcrumbs-->
+
+    <!--Action boxes-->
+    <div class="container-fluid">
+        <div class="row-fluid" style="background-color: white; min-height: 1000px; padding:10px;">
+            <div class="span12">
+                <div class="alert alert-danger" id="error" style="display: none;">
+                    This user already exist! Please Try Another
+                </div>
+
+                <div class="alert alert-success" id="success" style="display: none;">
+                    User Added Successfully
+                </div>
+                <div class="widget-box">
+
+                    <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
+                        <h5>Personal-info</h5>
+                    </div>
+                    <div class="widget-content nopadding">
+                        <form action="" name="form1" method="post" class="form-horizontal">
+                            <div class="control-group">
+                                <label class="control-label">User Name :</label>
+                                <div class="controls">
+                                    <input type="text" class="span11" placeholder="Username" name="username" />
+                                </div>
+                            </div>
+
+                            <div class="control-group">
+                                <label class="control-label">Password</label>
+                                <div class="controls">
+                                    <input type="password" class="span11" placeholder="Enter Password"
+                                        name="password" />
+                                </div>
+                            </div>
+
+                            <div class="control-group">
+                                <label class="control-label">Select Role</label>
+                                <div class="controls">
+                                    <select class="span11" name="role">
+                                        <option>user</option>
+                                        <option>admin</option>
+                                    </select>
+                                </div>
+                            </div>
+
+
+
+                            <div class="form-actions">
+                                <button type="submit" name="submit1" class="btn btn-success">Save</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="widget-content nopadding">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Username</th>
+                                <th>Role</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $res = mysqli_query($link, 'select * from users');
+                            while ($row = mysqli_fetch_array($res)) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $row['id'] ?></td>
+                                    <td><?php echo $row['username']?></td>
+                                    <td><?php echo $row['role']?></td>
+                                    <td>
+                                        <button class="btn btn-warning"><i class="icon-pencil"></i> Edit</button>
+                                        <a href="delete_user.php?id=<?php echo $row['id'] ?>" class="btn btn-danger"><i class="icon-trash"></i> Delete</a>
+                                    </td>
+
+                                </tr>
+                                <?php
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+if (isset($_POST['submit1'])) {
+    $count = 0;
+    $result = mysqli_query($link, "select * from users where username='$_POST[username]'");
+    $count = mysqli_num_rows($result);
+
+    if ($count > 0) {
+        ?>
+        <script type="text/javascript">
+            document.getElementById("success").style.display = "none";
+            document.getElementById("error").style.display = "block";
+            setTimeout(function(){
+                window.location.href =window.location.href;
+            }, 3000);
+        </script>
+        <?php
+    } else {
+        mysqli_query($link, "insert into users values(NULL, '$_POST[username]', '$_POST[password]', '$_POST[role]')");
+
+        ?>
+        <script type="text/javascript">
+            document.getElementById("error").style.display = "none";
+            document.getElementById("success").style.display = "block";
+            setTimeout(function(){
+                window.location.href =window.location.href;
+            }, 3000);
+        </script>
+        <?php
+    }
+
+}
+?>
+
+<!--end-main-container-part-->
+
+<?php
+include 'footer.php';
+?>
---
+++ user/index.php
@@ -0,0 +1,77 @@
+<?php 
+ include 'connection.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Login - php inventory management system</title>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/bootstrap-responsive.min.css"/>
+    <link rel="stylesheet" href="css/matrix-login.css"/>
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet"/>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+<div id="loginbox">
+    <form name="form1" class="form-vertical" action="" method="post">
+        <div class="control-group normal_text"><h3>Login Page</h3></div>
+        <div class="control-group">
+            <div class="controls">
+                <div class="main_input_box">
+                    <span class="add-on bg_lg"><i class="icon-user"> </i></span><input type="text" name="username"
+                                                                                       placeholder="Username"/>
+                </div>
+            </div>
+        </div>
+        <div class="control-group">
+            <div class="controls">
+                <div class="main_input_box">
+                    <span class="add-on bg_ly"><i class="icon-lock"></i></span><input type="password" name="password"
+                                                                                      placeholder="Password"/>
+                </div>
+            </div>
+        </div>
+        <div class="form-actions">
+
+            <input type="submit" name="submit1" value="Login" href="index.html" class="btn btn-success"/>
+
+        </div>
+    </form>
+
+    <?php 
+        if(isset($_POST['submit1'])) {
+            $username = mysqli_escape_string($link, $_POST['username']);
+            $password = mysqli_escape_string($link, $_POST['password']);
+
+            $count = 0;
+            $result = mysqli_query($link, "select * from users where username='$username' && password='$password' && role='user'");
+            $count = mysqli_num_rows($result);
+
+            if($count > 0) {
+                ?>
+                <script  type="text/javascript">
+                    window.location='demo.php';
+                </script>
+                <?php
+            } else {
+                ?>
+                    <div class="alert alert-danger">
+                        Invalid Username or Password
+                    </div>
+                <?php
+            }
+        } 
+    ?>
+
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="js/matrix.login.js"></script>
+</body>
+
+</html>
